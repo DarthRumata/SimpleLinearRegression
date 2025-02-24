92f143629f47595e52ad9df4f0a4e7c4dfbb5d69
Stanislav Kirichok <kirichok.stas@gmail.com>
2025-02-23 20:18:35 -0800

    Initial Commit

---
+++ SimpleLinearRegression/SimpleLinearRegressionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLinearRegressionApp.swift
+//  SimpleLinearRegression
+//
+//  Created by Stas Kirichok on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLinearRegressionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
